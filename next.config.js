4bd0210df3ade225542f0a63bf6b07bd74b7b6d9
Bilguun <Bilguun040728@gmail.com>
2025-04-21 10:26:10 +0800

    e

--- next.config.js
+++ next.config.js
@@ -1,22 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['res.cloudinary.com'],
+    domains: ["res.cloudinary.com"],
   },
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
-        destination: process.env.NODE_ENV === 'production' 
-          ? 'https://c16-mn.onrender.com/api/:path*'
-          : 'http://localhost:8000/api/:path*',
+        source: "/api/:path*",
+        destination:
+          process.env.NODE_ENV === "production"
+            ? "https://c16-mn.onrender.com/api/:path*"
+            : "http://localhost:8000/api/:path*",
       },
     ];
   },
-  // Set the port for production
-  env: {
-    PORT: process.env.PORT || 3000,
-  },
 };
 
-module.exports = nextConfig; 
+module.exports = nextConfig;
